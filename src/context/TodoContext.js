6b15038dde3c2c6f8e51aa97245441b480b9cf88
devhaseeb4 <devhaseeb4@gmail.com>
2024-08-04 04:32:38 +0000

    final clean-up

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,19 +1,6 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({
-  todos: [
-    {
-      id: 1,
-      title: "Learn React",
-      completed: false,
-    },
-  ],
-
-  addTodo: (todo) => {},
-  editTodo: (todo, id) => {},
-  removeTodo: (id) => {},
-  toggleComplete: (id) => {},
-});
+export const TodoContext = createContext();
 
 export const useTodo = () => {
   return useContext(TodoContext);
